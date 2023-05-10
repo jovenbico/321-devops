dac873d9cd1108f97060a35891e6e37d97ec3a83
Joven Bico <JovenBico@Gmail.com>
2023-05-10 09:49:58 +0000

    deploy tfe module

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -9,14 +9,19 @@ module "tfe" {
   organiztion  = local.organiztion
   github_token = var.GITHUB_TOKEN
 
+  aws_region            = var.AWS_REGION
+  aws_access_key_id     = var.AWS_ACCESS_KEY_ID
+  aws_secret_access_key = var.AWS_SECRET_ACCESS_KEY
+
   workspaces = {
     infra = {
-      branch         = "main",
-      identifier     = "jovenbico/321-devops"
+      branch     = "main",
+      identifier = "jovenbico/321-devops",
+      var_aws    = true
     },
     dev = {
-      branch         = "main",
-      identifier     = "jovenbico/321-devops"
+      branch     = "main",
+      identifier = "jovenbico/321-devops"
     }
   }
 
--- terraform/main/variables.tf
+++ terraform/main/variables.tf
@@ -10,5 +10,20 @@ variable "TERRAFORM_IO_ORGANIZATION" {
 
 variable "GITHUB_TOKEN" {
   description = "Github token"
-  type = string
+  type        = string
+}
+
+variable "AWS_REGION" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key id"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret access key"
+  type        = string
 }
