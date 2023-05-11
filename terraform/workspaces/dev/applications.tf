44a4f7c514f32fbddbd9008b89dc5e4ef7c6a889
Joven Bico <JovenBico@Gmail.com>
2023-05-11 07:33:15 +0000

    deploy applications

---
+++ terraform/workspaces/dev/applications.tf
@@ -0,0 +1,28 @@
+locals {
+  deploy_hello_app = true
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+resource "helm_release" "hello_app" {
+  count = (local.deploy_hello_app ? 1 : 0)
+
+  name  = "hello_app"
+  chart = "../../../applications/hello-world/chart"
+}
