65e5e43d07a0129f516e0dec7a7bd3b2881bcb14
Joven Bico <JovenBico@Gmail.com>
2023-05-12 09:02:51 +0000

    deploy metrics server to kube-system namespace

--- terraform/modules/addons/metrics-server/main.tf
+++ terraform/modules/addons/metrics-server/main.tf
@@ -6,6 +6,7 @@ resource "helm_release" "metrics_server" {
   count = (local.addon_metrics_server ? 1 : 0)
 
   name       = "metrics-server"
+  namespace  = "kube-system"
   repository = "https://kubernetes-sigs.github.io/metrics-server"
   chart      = "metrics-server/metrics-server"
   version    = "3.10.0"
