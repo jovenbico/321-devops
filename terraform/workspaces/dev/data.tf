91689adac6bc291a7fbc4fd461e572a142f99a10
Joven Bico <JovenBico@Gmail.com>
2023-05-15 08:20:53 +0000

    revert

--- terraform/workspaces/dev/data.tf
+++ terraform/workspaces/dev/data.tf
@@ -2,17 +2,17 @@
 data "aws_availability_zones" "available" {}
 
 data "aws_eks_cluster" "this" {
-  name = time_sleep.this.triggers["eks_cluster_name"]
+  name = time_sleep.this.triggers["cluster_name"]
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = time_sleep.this.triggers["eks_cluster_name"]
+  name = time_sleep.this.triggers["cluster_name"]
 }
 
 resource "time_sleep" "this" {
   create_duration = "60s"
 
   triggers = {
-    eks_cluster_name = module.eks.cluster_name # wait to be ready
+    cluster_name = module.eks.cluster_name # wait to be ready
   }
 }
