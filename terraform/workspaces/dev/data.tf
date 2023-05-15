abc80d3768cae69668c4f55f3f7e31b5a80370d2
Joven Bico <JovenBico@Gmail.com>
2023-05-15 08:16:47 +0000

    module.eks.cluster_name # wait to be ready

--- terraform/workspaces/dev/data.tf
+++ terraform/workspaces/dev/data.tf
@@ -2,17 +2,17 @@
 data "aws_availability_zones" "available" {}
 
 data "aws_eks_cluster" "this" {
-  name = time_sleep.this.triggers["eks_cluster_id"]
+  name = time_sleep.this.triggers["eks_cluster_name"]
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = time_sleep.this.triggers["eks_cluster_id"]
+  name = time_sleep.this.triggers["eks_cluster_name"]
 }
 
 resource "time_sleep" "this" {
   create_duration = "60s"
 
   triggers = {
-    eks_cluster_id = module.eks.cluster_id # wait to be ready
+    eks_cluster_name = module.eks.cluster_name # wait to be ready
   }
 }
