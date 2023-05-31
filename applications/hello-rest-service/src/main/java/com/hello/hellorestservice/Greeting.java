32a9c2ad4e604b4cbe710c238c1f53aa1f8dc2cd
Joven Bico <joven@frankiebico.com>
2023-05-31 06:38:49 +0000

    add java hello-rest-service

---
+++ applications/hello-rest-service/src/main/java/com/hello/hellorestservice/Greeting.java
@@ -0,0 +1,4 @@
+package com.hello.hellorestservice;
+
+public record Greeting(long id, String content) { }
+
---
+++ applications/hello-rest-service/src/test/java/com/hello/hellorestservice/RestServiceAppTests.java
@@ -0,0 +1,13 @@
+package com.hello.hellorestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
